f9557595bf0c0a0142d5c8546634ce4b212d7e59
sofia12k <145674708+sofia12k@users.noreply.github.com>
2025-04-05 01:01:50 +0300

    Create foundItems.directive.js

---
+++ foundItems.directive.js
@@ -0,0 +1,28 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp')
+.directive('foundItems', FoundItemsDirective);
+
+function FoundItemsDirective() {
+  var ddo = {
+    restrict: 'E',
+    template:
+      '<ul>' +
+        '<li ng-repeat="item in found">' +
+          '{{ item.name }} ({{ item.short_name }}) - {{ item.description }}' +
+          ' <button class="btn btn-danger btn-sm" ng-click="onRemove({index: $index})">' +
+            'Remove' +
+          '</button>' +
+        '</li>' +
+      '</ul>',
+    scope: {
+      found: '<',
+      onRemove: '&'
+    }
+  };
+
+  return ddo;
+}
+
+})();
