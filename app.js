0809e55ce6ef760c2ab0d65d135729f4c3d9af9f
sofia12k <145674708+sofia12k@users.noreply.github.com>
2025-04-05 14:21:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,86 +1,74 @@
 (function () {
-  'use strict';
+'use strict';
 
-  angular.module('NarrowItDownApp', [])
-    .controller('NarrowItDownController', NarrowItDownController)
-    .service('MenuSearchService', MenuSearchService)
-    .directive('foundItems', FoundItemsDirective);
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
 
-  // Controller Definition
-  NarrowItDownController.$inject = ['MenuSearchService'];
-  function NarrowItDownController(MenuSearchService) {
-    var ctrl = this;
-    ctrl.searchTerm = '';
-    ctrl.found = [];
-    ctrl.nothingFound = false;
-    ctrl.loading = false; // To track the loading state
+// ---------- Directive ----------
+function FoundItemsDirective() {
+  var ddo = {
+    restrict: 'E',
+    templateUrl: 'foundItems.html',
+    scope: {
+      found: '<',
+      onRemove: '&'
+    }
+  };
+  return ddo;
+}
 
-    // Function to narrow down menu items
-    ctrl.narrowDown = function () {
-      var term = ctrl.searchTerm.trim();
-      if (!term) {
-        ctrl.found = [];
-        ctrl.nothingFound = true;
-        return;
+// ---------- Controller ----------
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var ctrl = this;
+
+  ctrl.searchTerm = '';
+  ctrl.found = [];
+  ctrl.searched = false;
+
+  ctrl.getMatchedMenuItems = function () {
+    ctrl.searched = true;
+    if (!ctrl.searchTerm || ctrl.searchTerm.trim() === '') {
+      ctrl.found = [];
+      return;
+    }
+
+    MenuSearchService.getMatchedMenuItems(ctrl.searchTerm)
+    .then(function (foundItems) {
+      ctrl.found = foundItems;
+    });
+  };
+
+  ctrl.removeItem = function (itemIndex) {
+    ctrl.found.splice(itemIndex, 1);
+  };
+}
+
+// ---------- Service ----------
+MenuSearchService.$inject = ['$http'];
+function MenuSearchService($http) {
+  var service = this;
+
+  service.getMatchedMenuItems = function (searchTerm) {
+    return $http({
+      method: 'GET',
+      url: 'https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json'
+    }).then(function (response) {
+      var allItems = response.data.menu_items;
+      var foundItems = [];
+
+      for (var i = 0; i < allItems.length; i++) {
+        var item = allItems[i];
+        if (item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
+          foundItems.push(item);
+        }
       }
 
-      ctrl.loading = true; // Show loader while waiting for response
-
-      MenuSearchService.getMatchedMenuItems(term)
-        .then(function (items) {
-          ctrl.found = items;
-          ctrl.nothingFound = items.length === 0;
-        })
-        .finally(function () {
-          ctrl.loading = false; // Hide loader when the data is ready
-        });
-    };
-
-    // Function to remove an item from the found list
-    ctrl.removeItem = function (index) {
-      ctrl.found.splice(index, 1);
-    };
-  }
-
-  // Service to retrieve menu items from Firebase and filter them based on search term
-  MenuSearchService.$inject = ['$http'];
-  function MenuSearchService($http) {
-    var service = this;
-
-    service.getMatchedMenuItems = function (searchTerm) {
-      return $http.get('https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json')
-        .then(function (response) {
-          var allItems = response.data.menu_items;
-          var foundItems = allItems.filter(function (item) {
-            return item.description.toLowerCase().includes(searchTerm.toLowerCase());
-          });
-          return foundItems;
-        });
-    };
-  }
-
-  // Directive to display the found items
-  function FoundItemsDirective() {
-    return {
-      restrict: 'E',
-      scope: {
-        foundItems: '<',
-        onRemove: '&'
-      },
-      template: `
-        <ul class="list-group mt-3" ng-if="foundItems.length > 0">
-          <li class="list-group-item d-flex justify-content-between align-items-start"
-              ng-repeat="item in foundItems track by $index">
-            <div>
-              <strong>{{ item.name }}</strong> ({{ item.short_name }})<br>
-              <small>{{ item.description }}</small>
-            </div>
-            <button class="btn btn-danger btn-sm" ng-click="onRemove({index: $index})">
-              Don't want this one!
-            </button>
-          </li>
-        </ul>`
-    };
-  }
+      return foundItems;
+    });
+  };
+}
 
 })();
