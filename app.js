4387a9af8c4728f985b45fddabbffcaffec8c3aa
sofia12k <145674708+sofia12k@users.noreply.github.com>
2025-04-05 13:48:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,77 +1,72 @@
 (function () {
-'use strict';
+  'use strict';
 
-angular.module('NarrowItDownApp', [])
-.controller('NarrowItDownController', NarrowItDownController)
-.service('MenuSearchService', MenuSearchService)
-.directive('foundItems', FoundItemsDirective);
+  angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', FoundItemsDirective);
 
-function FoundItemsDirective() {
-  var ddo = {
-    restrict: 'E',
-    templateUrl: 'foundItems.html',
-    scope: {
-      found: '<',
-      onRemove: '&'
-    }
-  };
-  return ddo;
-}
-
-NarrowItDownController.$inject = ['MenuSearchService'];
-function NarrowItDownController(MenuSearchService) {
-  var ctrl = this;
-  ctrl.searchTerm = '';
-  ctrl.found = [];
-  ctrl.nothingFound = false;
-
-  ctrl.narrowItDown = function () {
+  // === Controller ===
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var ctrl = this;
+    ctrl.searchTerm = '';
+    ctrl.found = [];
     ctrl.nothingFound = false;
-    if (!ctrl.searchTerm || ctrl.searchTerm.trim() === '') {
-      ctrl.found = [];
-      ctrl.nothingFound = true;
-      return;
-    }
 
-    MenuSearchService.getMatchedMenuItems(ctrl.searchTerm)
-      .then(function (foundItems) {
-        ctrl.found = foundItems;
-        ctrl.nothingFound = foundItems.length === 0;
-      })
-      .catch(function (error) {
-        console.error(error);
+    ctrl.narrowDown = function () {
+      if (!ctrl.searchTerm) {
         ctrl.found = [];
         ctrl.nothingFound = true;
-      });
-  };
-
-  ctrl.removeItem = function (index) {
-    ctrl.found.splice(index, 1);
-  };
-}
-
-MenuSearchService.$inject = ['$http'];
-function MenuSearchService($http) {
-  var service = this;
-
-  service.getMatchedMenuItems = function (searchTerm) {
-    return $http({
-      method: 'GET',
-      url: 'https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json'
-    }).then(function (response) {
-      var allItems = response.data.menu_items;
-      var matchedItems = [];
-
-      for (var i = 0; i < allItems.length; i++) {
-        var item = allItems[i];
-        if (item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
-          matchedItems.push(item);
-        }
+        return;
       }
-      return matchedItems;
-    });
-  };
-}
+
+      MenuSearchService.getMatchedMenuItems(ctrl.searchTerm)
+        .then(function (items) {
+          ctrl.found = items;
+          ctrl.nothingFound = items.length === 0;
+        });
+    };
+
+    ctrl.removeItem = function (index) {
+      ctrl.found.splice(index, 1);
+    };
+  }
+
+  // === Service ===
+  MenuSearchService.$inject = ['$http'];
+  function MenuSearchService($http) {
+    var service = this;
+
+    service.getMatchedMenuItems = function (searchTerm) {
+      return $http.get('https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json')
+        .then(function (response) {
+          var allItems = response.data.menu_items;
+          var foundItems = allItems.filter(function (item) {
+            return item.description.toLowerCase().includes(searchTerm.toLowerCase());
+          });
+          return foundItems;
+        });
+    };
+  }
+
+  // === Directive ===
+  function FoundItemsDirective() {
+    return {
+      restrict: 'E',
+      scope: {
+        foundItems: '<',
+        onRemove: '&'
+      },
+      template: `
+        <ul class="list-group">
+          <li class="list-group-item" ng-repeat="item in foundItems track by $index">
+            {{ item.name }}, {{ item.short_name }}, {{ item.description }}
+            <button class="btn btn-danger btn-sm float-end" ng-click="onRemove({index: $index})">Don't want this one!</button>
+          </li>
+        </ul>
+      `
+    };
+  }
 
 })();
-
