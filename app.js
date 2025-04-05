9df0ade9a17d5a692e5196ce4dd2c24c82c2451b
sofia12k <145674708+sofia12k@users.noreply.github.com>
2025-04-05 14:37:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,88 +1,76 @@
 (function () {
-  'use strict';
+'use strict';
 
-  angular.module('NarrowItDownApp', [])
-    .controller('NarrowItDownController', NarrowItDownController)
-    .service('MenuSearchService', MenuSearchService)
-    .directive('foundItems', FoundItemsDirective);
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
 
-  // Directive
-  function FoundItemsDirective() {
-    var ddo = {
-      restrict: 'E',
-      templateUrl: 'foundItems.html',
-      scope: {
-        found: '<',
-        onRemove: '&'
-      }
-    };
-    return ddo;
-  }
+// Directive
+function FoundItemsDirective() {
+  return {
+    restrict: 'E',
+    templateUrl: 'foundItems.html',
+    scope: {
+      found: '<',
+      onRemove: '&'
+    }
+  };
+}
 
-  // Controller
-  NarrowItDownController.$inject = ['MenuSearchService'];
-  function NarrowItDownController(MenuSearchService) {
-    var ctrl = this;
+// Controller
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var ctrl = this;
 
-    ctrl.searchTerm = '';
+  ctrl.searchTerm = '';
+  ctrl.found = [];
+  ctrl.searched = false;
+  ctrl.loading = false;
+
+  ctrl.getMatchedMenuItems = function () {
     ctrl.found = [];
-    ctrl.searched = false;
-    ctrl.loading = false;
+    ctrl.searched = true;
+    ctrl.loading = true;
 
-    ctrl.getMatchedMenuItems = function () {
-      ctrl.found = [];
-      ctrl.searched = false;
-      ctrl.loading = true;
+    var trimmedTerm = ctrl.searchTerm.trim();
+    if (!trimmedTerm) {
+      ctrl.loading = false;
+      return;
+    }
 
-      if (!ctrl.searchTerm || ctrl.searchTerm.trim() === '') {
+    MenuSearchService.getMatchedMenuItems(trimmedTerm)
+      .then(function (foundItems) {
+        ctrl.found = foundItems;
+      })
+      .finally(function () {
         ctrl.loading = false;
-        ctrl.searched = true;
-        return;
-      }
-
-      MenuSearchService.getMatchedMenuItems(ctrl.searchTerm)
-        .then(function (foundItems) {
-          console.log(foundItems); // Log found items to verify the data
-          ctrl.found = foundItems;
-          ctrl.loading = false;
-          ctrl.searched = true;
-        });
-    };
-
-    ctrl.removeItem = function (itemIndex) {
-      ctrl.found.splice(itemIndex, 1);
-    };
-  }
-
-  // Service
-  MenuSearchService.$inject = ['$http'];
-  function MenuSearchService($http) {
-    var service = this;
-
-    service.getMatchedMenuItems = function (searchTerm) {
-      return $http({
-        method: "GET",
-        url: "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json"
-      }).then(function (result) {
-        console.log(result.data); // Log API response to ensure the structure is correct
-
-        if (result.data && result.data.menu_items) {
-          var allItems = result.data.menu_items;
-          var foundItems = [];
-
-          for (var i = 0; i < allItems.length; i++) {
-            if (allItems[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
-              foundItems.push(allItems[i]);
-            }
-          }
-
-          return foundItems;
-        } else {
-          console.error('No menu items found in response');
-          return [];
-        }
       });
-    };
-  }
+  };
+
+  ctrl.removeItem = function (index) {
+    ctrl.found.splice(index, 1);
+  };
+}
+
+// Service
+MenuSearchService.$inject = ['$http'];
+function MenuSearchService($http) {
+  var service = this;
+
+  service.getMatchedMenuItems = function (searchTerm) {
+    return $http({
+      method: "GET",
+      url: "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json"
+    }).then(function (response) {
+      var menuItems = response.data.menu_items || [];
+      var foundItems = menuItems.filter(function (item) {
+        return item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+      });
+      return foundItems;
+    });
+  };
+}
 
 })();
+
